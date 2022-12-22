b3b6311dc891ec2072df15369078bb359ff2aced
Pavel-Troinich <tr.pav.ar@gmail.com>
2022-12-22 11:51:15 +0300

    routing

---
+++ index.js
@@ -0,0 +1,25 @@
+const route = (event) => {
+  event = event || window.event;
+  event.preventDefault();
+  window.history.pushState({}, '', event.target.href);
+  handleLocation();
+};
+
+const routes = {
+  404: '/404.html',
+  '/': '/home.html',
+  '/cart': '/cart.html',
+  '/about': '/about.html',
+};
+
+const handleLocation = async () => {
+  const path = window.location.pathname;
+  console.log(path);
+  const route = routes[path] || routes[404];
+  const html = await fetch(route).then((data) => data.text());
+  document.getElementById('main-page').innerHTML = html;
+};
+
+window.onpopstate = handleLocation;
+window.route = route;
+handleLocation();
