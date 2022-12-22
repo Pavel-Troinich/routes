3b44bad6a09a0c6baf26bce4264b1d4acd61fd27
Pavel <95251998+Pavel-Troinich@users.noreply.github.com>
2022-12-22 12:23:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const route = (event) => {
 
 const routes = {
   404: '/routes/404.html',
-  '/': '/routes/home.html',
-  '/cart': '/routes/cart.html',
-  '/about': '/routes/about.html',
+  '/routes/': '/routes/home.html',
+  '/routes/cart': '/routes/cart.html',
+  '/routes/about': '/routes/about.html',
 };
 
 const handleLocation = async () => {
